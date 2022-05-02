a25ba7aaf2c1516df4191e950bd67cf33323e01f
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-02 00:25:47 -0400

    Implement get all items test

--- VendingMachine/src/test/java/com/sg/vendingmachine/dao/VendDaoFileImplTest.java
+++ VendingMachine/src/test/java/com/sg/vendingmachine/dao/VendDaoFileImplTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.FileWriter;
 import java.math.BigDecimal;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class VendDaoFileImplTest {
 
@@ -46,6 +47,37 @@ class VendDaoFileImplTest {
                 "Checking item cost.");
     }
 
+    @Test
+    public void testGetAllItems() throws Exception {
+        ItemDto firstItem = new ItemDto("1");
+        firstItem.setName("Chips");
+        firstItem.setCost(new BigDecimal("9.99"));
+        firstItem.setStock(10);
 
+        ItemDto secondItem = new ItemDto("2");
+        secondItem.setName("Granola Bar");
+        secondItem.setCost(new BigDecimal("1.03"));
+        secondItem.setStock(5);
+
+        testDao.addItem(firstItem.getId(), firstItem);
+        testDao.addItem(secondItem.getId(), secondItem);
+
+        List<ItemDto> allItems = testDao.getAllItems();
+
+        assertNotNull(allItems, "The list of items must not be null.");
+        assertEquals(2, allItems.size(), "The list of items should have 2 items.");
+
+        assertTrue(testDao.getAllItems().contains(firstItem), "The list of items should include Chips.");
+        assertTrue(testDao.getAllItems().contains(secondItem), "The list of items should include Granola Bar.");
+    }
+
+    @Test
+    public void testGetReduceItemStock() throws Exception {}
+
+    @Test
+    public void testGetItemCost() throws Exception {}
+
+    @Test
+    public void testSubtractMoney() throws Exception {}
 
 }
