81ed5794daa9aaf57aa4590b31dcc968f1b3ae38
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-02 10:21:43 -0400

    Implement transaction and change services

---
+++ VendingMachine/src/main/java/com/sg/vendingmachine/service/ChangeService.java
@@ -0,0 +1,64 @@
+package com.sg.vendingmachine.service;
+
+import com.sg.vendingmachine.Change;
+
+import java.math.BigDecimal;
+
+public class ChangeService {
+
+    public String returnChange(BigDecimal amount) {
+        Change change = new Change(amount);
+        int quarters = change.returnQuarters();
+        int dimes = change.returnDimes();
+        int nickels = change.returnNickels();
+        int pennies = change.returnPennies();
+        String moneyReturned = "Your change: ";
+
+        moneyReturned += returnQuarters(quarters);
+        moneyReturned += returnDimes(dimes);
+        moneyReturned += returnNickels(nickels);
+        moneyReturned += ;
+
+        return moneyReturned;
+    }
+
+    private String returnQuarters(int quarters) {
+        String quartersReturned = "";
+        if (quarters > 1) {
+            quartersReturned += "\n" + quarters + " Quarters";
+        } else if (quarters == 1) {
+            quartersReturned += "\n" + quarters + " Quarter";
+        }
+        return quartersReturned;
+    }
+
+    private String returnDimes(int dimes) {
+        String dimesReturned = "";
+        if (dimes > 1) {
+            dimesReturned += "\n" + dimes + " Dimes";
+        } else if (dimes == 1) {
+            dimesReturned += "\n" + dimes + " Dime";
+        }
+        return dimesReturned;
+    }
+
+    private String returnNickels(int nickels) {
+        String nickelsReturned = "";
+        if (nickels > 1) {
+            nickelsReturned += "\n" + nickels + " Nickels";
+        } else if (nickels == 1) {
+            nickelsReturned += "\n" + nickels + " Nickel";
+        }
+        return nickelsReturned;
+    }
+
+    private String returnPennies(int pennies) {
+        String penniesReturned = "";
+        if (pennies == 1) {
+            penniesReturned += "\n" + pennies + " Penny";
+        } else {
+            penniesReturned += "\n" + pennies + " Pennies";
+        }
+    }
+
+}
---
+++ VendingMachine/src/main/java/com/sg/vendingmachine/service/TransactionService.java
@@ -0,0 +1,49 @@
+package com.sg.vendingmachine.service;
+
+import com.sg.vendingmachine.dao.VendAuditDao;
+import com.sg.vendingmachine.dao.VendDao;
+import com.sg.vendingmachine.dao.VendPersistenceException;
+
+import java.math.BigDecimal;
+
+public class TransactionService {
+    private VendDao dao;
+    private VendAuditDao auditDao;
+    private ChangeService changeService;
+
+    public TransactionService(VendDao dao, VendAuditDao auditDao, ChangeService changeService) {
+        this.dao = dao;
+        this.auditDao = auditDao;
+        this.changeService = changeService;
+    }
+
+    public void checkStock(String itemId) throws VendPersistenceException, VendNoItemInventoryException {
+        if (dao.getItem(itemId).getStock() <= 0) {
+            throw new VendNoItemInventoryException("ERROR: Item is out of stock.");
+        }
+    }
+
+    public void compareMoney(BigDecimal userMoney, String itemId) throws VendPersistenceException, VendInsufficientFundsException {
+        if (userMoney.compareTo(dao.getItemCost(itemId)) < 0) {
+            throw new VendInsufficientFundsException("ERROR: Insufficient funds.");
+        }
+    }
+
+    public BigDecimal subtractMoney(BigDecimal moneyInserted, String itemId) throws VendPersistenceException,
+            VendNoItemInventoryException,
+            VendInsufficientFundsException{
+        compareMoney(moneyInserted, itemId);
+        checkStock(itemId);
+        moneyInserted = dao.subtractMoney(moneyInserted, itemId);
+        dao.reduceItemStock(itemId);
+
+        auditDao.writeAuditEntry("1 " + dao.getItem(itemId).getName() + " sold for "
+                + dao.getItemCost(itemId) + ". " + dao.getItemStock(itemId) +
+                " remaining. Change returned: $" + moneyInserted);
+        return moneyInserted;
+    }
+
+    public String getChange(BigDecimal amount) {
+        return changeService.returnChange(amount);
+    }
+}
