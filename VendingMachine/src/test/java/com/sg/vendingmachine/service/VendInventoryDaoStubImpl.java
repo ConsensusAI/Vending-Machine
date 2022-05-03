6d41403fa1874354d4c6493d0322c9da1435027c
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-02 21:49:09 -0400

    Refactor inventory dao stub

--- VendingMachine/src/test/java/com/sg/vendingmachine/service/VendInventoryDaoStubImpl.java
+++ VendingMachine/src/test/java/com/sg/vendingmachine/service/VendInventoryDaoStubImpl.java
@@ -1,80 +1,45 @@
-//package com.sg.vendingmachine.service;
-//
-//import com.sg.vendingmachine.inventory.VendInventoryDao;
-//import com.sg.vendingmachine.audit.AuditPersistenceException;
-//import com.sg.vendingmachine.inventory.ItemDto;
-//
-//import java.math.BigDecimal;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class VendInventoryDaoStubImpl implements VendInventoryDao {
-//
-//    public ItemDto onlyItem;
-//
-//    public VendInventoryDaoStubImpl() {
-//        onlyItem = new ItemDto("1");
-//        onlyItem.setName("Chips");
-//        onlyItem.setCost(new BigDecimal("9.99"));
-//        onlyItem.setStock(10);
-//    }
-//
-//    public VendInventoryDaoStubImpl(ItemDto testItem) {
-//        this.onlyItem = testItem;
-//    }
-//
-//    @Override
-//    public List<ItemDto> getAllItems() throws AuditPersistenceException {
-//        List<ItemDto> allItems = new ArrayList<>();
-//        allItems.add(onlyItem);
-//        return allItems;
-//    }
-//
-//    @Override
-//    public ItemDto getItem(String id) throws AuditPersistenceException {
-//        if (id.equals(onlyItem.getId())) {
-//            return onlyItem;
-//        }
-//        return null;
-//    }
-//
-//    @Override
-//    public int getItemStock(String id) throws AuditPersistenceException {
-//        if (id.equals(onlyItem.getId())) {
-//            return onlyItem.getStock();
-//        }
-//        return -1;
-//    }
-//
-//    @Override
-//    public void reduceItemStock(String id) throws AuditPersistenceException {
-//        if (id.equals(onlyItem.getId())) {
-//            onlyItem.reduceStock();
-//        }
-//    }
-//
-//    @Override
-//    public BigDecimal getItemCost(String id) throws AuditPersistenceException {
-//        if (id.equals(onlyItem.getId())) {
-//            return onlyItem.getCost();
-//        }
-//        return null;
-//    }
-//
-//    @Override
-//    public BigDecimal subtractMoney(BigDecimal initMoney, String itemId) {
-//        if (itemId.equals(onlyItem.getId())) {
-//            BigDecimal cost = onlyItem.getCost();
-//            return initMoney.subtract(cost);
-//        }
-//        return null;
-//    }
-//
-//    @Override
-//    public ItemDto addItem(String id, ItemDto item) throws AuditPersistenceException {
-//        if (id.equals(onlyItem.getId())) {
-//            return onlyItem;
-//        }
-//        return null;
-//    }
-//}
+package com.sg.vendingmachine.service;
+
+import com.sg.vendingmachine.inventory.InventoryDao;
+import com.sg.vendingmachine.inventory.InventoryPersistenceException;
+import com.sg.vendingmachine.inventory.ItemDto;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VendInventoryDaoStubImpl implements InventoryDao {
+
+    public ItemDto onlyItem;
+
+    public VendInventoryDaoStubImpl() {
+        onlyItem = new ItemDto("1");
+        onlyItem.setName("Chips");
+        onlyItem.setCost(new BigDecimal("9.99"));
+        onlyItem.setStock(10);
+    }
+
+    public VendInventoryDaoStubImpl(ItemDto testItem) {
+        this.onlyItem = testItem;
+    }
+
+    @Override
+    public List<ItemDto> getAllItems() throws InventoryPersistenceException {
+        List<ItemDto> allItems = new ArrayList<>();
+        allItems.add(onlyItem);
+        return allItems;
+    }
+
+    @Override
+    public ItemDto getItem(String id) throws InventoryPersistenceException {
+        if (id.equals(onlyItem.getId())) {
+            return onlyItem;
+        }
+        return null;
+    }
+
+    @Override
+    public void updateItem(String id, ItemDto item) throws InventoryPersistenceException {
+
+    }
+}
