e766a7278aae3db73fad1eebdbb82eed9f4da5f5
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-02 20:53:22 -0400

    Implement Transaction Service to handle only transaction logic

--- VendingMachine/src/main/java/com/sg/vendingmachine/service/TransactionService.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/service/TransactionService.java
@@ -1,30 +1,52 @@
 package com.sg.vendingmachine.service;
 
+import com.sg.vendingmachine.Change;
+import com.sg.vendingmachine.dao.InventoryPersistenceException;
 import com.sg.vendingmachine.dao.VendAuditDao;
 import com.sg.vendingmachine.dao.VendInventoryDao;
 import com.sg.vendingmachine.dao.AuditPersistenceException;
+import com.sg.vendingmachine.dto.ItemDto;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 public class TransactionService {
-    private final VendInventoryDao inventoryDao;
-    private final VendAuditDao auditDao;
+    private final InventoryService inventoryService;
+    private final AuditService auditService;
     private final ChangeService changeService;
 
-    public TransactionService(VendInventoryDao inventoryDao, VendAuditDao auditDao) {
-        this.inventoryDao = inventoryDao;
-        this.auditDao = auditDao;
+    public TransactionService(InventoryService inventoryService, AuditService auditService) {
+        this.inventoryService = inventoryService;
+        this.auditService = auditService;
         this.changeService = new ChangeService();
     }
 
-    public void checkStock(String itemId) throws AuditPersistenceException, NoStockException {
-        if (inventoryDao.getItem(itemId).getStock() <= 0) {
+    public void checkStock(ItemDto item) throws NoStockException {
+        if (item.getStock() <= 0) {
             throw new NoStockException("ERROR: Item is out of stock.");
         }
     }
 
-    public void compareMoney(BigDecimal userMoney, String itemId) throws AuditPersistenceException, InsufficientFundsException {
-        if (userMoney.compareTo(inventoryDao.getItemCost(itemId)) < 0) {
+    public Optional<Change> computeTransaction(BigDecimal moneyInserted, String itemId)
+    throws InventoryPersistenceException, AuditPersistenceException, NoStockException, InsufficientFundsException {
+        ItemDto item = inventoryService.getItem(itemId);
+        checkStock(item);
+        compareMoney(moneyInserted, item);
+
+        inventoryService.reduceItemStock(itemId);
+
+        Optional<Change> changeMaybe = Optional.empty();
+        BigDecimal amountToReturn = moneyInserted.subtract(item.getCost());
+        if (amountToReturn.compareTo(BigDecimal.ZERO) > 0) { // Checks if amount to return is greater than 0.00
+            changeMaybe = Optional.of(changeService.computeChange(amountToReturn));
+        }
+
+        auditService.auditTransaction(item, changeMaybe);
+        return changeMaybe;
+    }
+
+    public void compareMoney(BigDecimal moneyInserted, ItemDto item) throws AuditPersistenceException, InsufficientFundsException {
+        if (moneyInserted.compareTo(item.getCost()) < 0) {
             throw new InsufficientFundsException("ERROR: Insufficient funds.");
         }
     }
@@ -34,11 +56,11 @@ public class TransactionService {
             InsufficientFundsException {
         compareMoney(moneyInserted, itemId);
         checkStock(itemId);
-        moneyInserted = inventoryDao.subtractMoney(moneyInserted, itemId);
-        inventoryDao.reduceItemStock(itemId);
+        moneyInserted = inventoryService.subtractMoney(moneyInserted, itemId);
+        inventoryService.reduceItemStock(itemId);
 
-        auditDao.writeAuditEntry("1 " + inventoryDao.getItem(itemId).getName() + " sold for "
-                + inventoryDao.getItemCost(itemId) + ". " + inventoryDao.getItemStock(itemId) +
+        auditService.writeAuditEntry("1 " + inventoryService.getItem(itemId).getName() + " sold for "
+                + inventoryService.getItemCost(itemId) + ". " + inventoryService.getItemStock(itemId) +
                 " remaining. Change returned: $" + moneyInserted);
         return moneyInserted;
     }
