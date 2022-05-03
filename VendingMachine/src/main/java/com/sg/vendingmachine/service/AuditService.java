65e963c516aa6014faad238a5483f643667cefb0
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-02 20:12:02 -0400

    Implement item mapper class to handle to DTO and to text functions

---
+++ VendingMachine/src/main/java/com/sg/vendingmachine/service/AuditService.java
@@ -0,0 +1,19 @@
+package com.sg.vendingmachine.service;
+
+import com.sg.vendingmachine.Change;
+import com.sg.vendingmachine.dao.VendAuditDao;
+import com.sg.vendingmachine.dto.ItemDto;
+
+import java.util.Optional;
+
+public class AuditService {
+    private final VendAuditDao auditDao;
+
+    public AuditService(VendAuditDao auditDao) {
+        this.auditDao = auditDao;
+    }
+
+    public void auditTransaction(ItemDto item, Optional<Change> changeMaybe) {
+
+    }
+}
