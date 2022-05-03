e64906282ee8fbd420d1f89fe5dc311dccb74303
ConsensusAI <malik.iqbal@hotmail.ca>
2022-05-02 21:01:49 -0400

    Refactor controller to sell one item at a time, send inventory logic to inventory service and send transaction logic to transaction service

--- VendingMachine/src/main/java/com/sg/vendingmachine/controller/VendController.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/controller/VendController.java
@@ -1,6 +1,8 @@
 package com.sg.vendingmachine.controller;
 
+import com.sg.vendingmachine.Change;
 import com.sg.vendingmachine.dao.AuditPersistenceException;
+import com.sg.vendingmachine.dao.InventoryPersistenceException;
 import com.sg.vendingmachine.dto.ItemDto;
 import com.sg.vendingmachine.service.*;
 import com.sg.vendingmachine.ui.VendView;
@@ -8,6 +10,7 @@ import com.sg.vendingmachine.ui.VendView;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
+import java.util.Optional;
 
 public class VendController {
 
@@ -44,43 +47,31 @@ public class VendController {
             }
 
             exitMessage();
-        } catch (AuditPersistenceException |
+        } catch (InventoryPersistenceException |
                 NoStockException |
-                InsufficientFundsException e) {
+                InsufficientFundsException |
+                AuditPersistenceException e) {
             view.printErrorMessage(e.getMessage());
         }
 
     }
 
-    private void purchaseItem() throws AuditPersistenceException,
+    private void purchaseItem() throws InventoryPersistenceException,
             NoStockException,
-            InsufficientFundsException {
+            InsufficientFundsException,
+            AuditPersistenceException {
         BigDecimal moneyInserted = view.promptMoneyInserted().setScale(2, RoundingMode.HALF_UP);
-        int itemSelection;
-        boolean itemPurchased = false;
-        List<ItemDto> allItems;
 
-        while (true) {
-            allItems = inventoryService.getAllItems();
-            view.printInventory(allItems);
-            view.printMoney(moneyInserted);
-            itemSelection = view.printItemsAndGetSelection(allItems);
-            if (itemSelection == allItems.size() + 1) {
-                break;
-            }
-
-            String itemId = String.valueOf(itemSelection);
-            moneyInserted = subtractMoney(moneyInserted, itemId);
-            itemPurchased = true;
+        List<ItemDto> allItems = inventoryService.getAllItems();
+        view.printMoney(moneyInserted);
+        int itemSelection = view.printItemsAndGetSelection(allItems);
+        if (itemSelection == allItems.size() + 1) {
+            return;
         }
 
-        if (itemPurchased) {
-            view.printChange(transactionService.getChange(moneyInserted));
-        }
-    }
-
-    private BigDecimal subtractMoney(BigDecimal moneyInserted, String itemId) throws AuditPersistenceException, NoStockException, InsufficientFundsException {
-        return transactionService.subtractMoney(moneyInserted, itemId);
+        String itemId = String.valueOf(itemSelection);
+        Optional<Change> changeMaybe = transactionService.computeTransaction(moneyInserted, itemId);
+        changeMaybe.ifPresentOrElse(view::printChange, view::printNoChange);
     }
 
     private void unknown() {
